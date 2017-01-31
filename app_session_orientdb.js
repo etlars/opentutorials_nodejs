5ea7dd21af4740ce0f29393817e1222e531d6539
Joseph Park <etlars@gmail.com>
2017-01-31 13:58:17 +0900

    session 9 : session store - orientdb
    db 접속 : 2424
    db 웹 접속 : 2480
    
    root@COLOR-ubuntu:~# sh /opt/orientdb/bin/server.sh
               .
              .`        `
              ,      `:.
             `,`    ,:`
             .,.   :,,
             .,,  ,,,
        .    .,.:::::  ````                                 :::::::::     :::::::::
        ,`   .::,,,,::.,,,,,,`;;                      .:    ::::::::::    :::    :::
        `,.  ::,,,,,,,:.,,.`  `                       .:    :::      :::  :::     :::
         ,,:,:,,,,,,,,::.   `        `         ``     .:    :::      :::  :::     :::
          ,,:.,,,,,,,,,: `::, ,,   ::,::`   : :,::`  ::::   :::      :::  :::    :::
           ,:,,,,,,,,,,::,:   ,,  :.    :   ::    :   .:    :::      :::  :::::::
            :,,,,,,,,,,:,::   ,,  :      :  :     :   .:    :::      :::  :::::::::
      `     :,,,,,,,,,,:,::,  ,, .::::::::  :     :   .:    :::      :::  :::     :::
      `,...,,:,,,,,,,,,: .:,. ,, ,,         :     :   .:    :::      :::  :::     :::
        .,,,,::,,,,,,,:  `: , ,,  :     `   :     :   .:    :::      :::  :::     :::
          ...,::,,,,::.. `:  .,,  :,    :   :     :   .:    :::::::::::   :::     :::
               ,::::,,,. `:   ,,   :::::    :     :   .:    :::::::::     ::::::::::
               ,,:` `,,.
              ,,,    .,`
             ,,.     `,                                          GRAPH DATABASE
           ``        `.
                     ``                                          orientdb.com
                     `
    
    2017-01-26 08:56:33:608 WARNI Not enough physical memory available for DISKCACHE: 986MB (heap=494MB). Set lower Maximum Heap (-Xmx setting on JVM) and restart OrientDB. Now running with DISKCACHE=256MB [orientechnologies]
    2017-01-26 08:56:33:616 INFO  OrientDB config DISKCACHE=-1,557MB (heap=494MB os=986MB disk=7,065MB) [orientechnologies]
    2017-01-26 08:56:34:286 INFO  Loading configuration from: /opt/orientdb/config/orientdb-server-config.xml... [OServerConfigurationLoaderXml]
    2017-01-26 08:56:35:303 INFO  OrientDB Server v2.1.3 (build UNKNOWN@r; 2015-10-04 10:56:30+0000) is starting up... [OServer]
    2017-01-26 08:56:35:387 INFO  Databases directory: /opt/orientdb/databases [OServer]
    2017-01-26 08:56:35:527 INFO  Listening binary connections on 0.0.0.0:2424 (protocol v.32, socket=default) [OServerNetworkListener]
    2017-01-26 08:56:35:541 INFO  Listening http connections on 0.0.0.0:2480 (protocol v.10, socket=default) [OServerNetworkListener]
    2017-01-26 08:56:35:601 INFO  Installing dynamic plugin 'studio-2.1.zip'... [OServerPluginManager]
    2017-01-26 08:56:35:773 INFO  Installing dynamic plugin 'orientdb-lucene-2.1.3-dist.jar'... [OServerPluginManager]
    2017-01-26 08:56:35:944 INFO  Lucene index plugin installed and active. Lucene version: LUCENE_47 [OLuceneIndexPlugin]
    2017-01-26 08:56:36:014 INFO  Installing GREMLIN language v.2.6.0 - graph.pool.max=50 [OGraphServerHandler]
    2017-01-26 08:56:36:015 INFO  [OVariableParser.resolveVariables] Error on resolving property: distributed [orientechnologies]
    2017-01-26 08:56:36:017 INFO  Installing Script interpreter. WARN: authenticated clients can execute any kind of code into the server by using the following allowed languages: [sql] [OServerSideScriptInterpreter]
    2017-01-26 08:56:36:017 INFO  OrientDB Server v2.1.3 (build UNKNOWN@r; 2015-10-04 10:56:30+0000) is active. [OServer]
    2017-01-26 08:57:14:986 WARNI ->10.1.0.1: Command not found: GET.sqlProfiler/color [ONetworkProtocolHttpDb]
    2017-01-26 09:13:26:392 WARNI ->10.1.0.1: Command not found: GET.sqlProfiler/color [ONetworkProtocolHttpDb]

---
+++ app_session_orientdb.js
@@ -0,0 +1,107 @@
+var express=require('express');
+var session=require('express-session');
+var OrientoStore = require('connect-oriento')(session);
+var bodyParser=require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+  secret: 'keyboard puppy',
+  resave: false,
+  saveUninitialized: true,
+  store:new OrientoStore({
+            server: "host=localhost&port=2424&username=root&password=dlsdus&db=color"
+        })
+}));
+
+// ROUTERS
+app.get('/count', function(req, res){
+  if(req.session.count){
+    req.session.count++;
+  }else{
+    req.session.count=1;
+  }
+  res.send('count: '+req.session.count);
+});
+
+app.get('/temp', function(req, res){
+  res.send('result: '+count);
+});
+
+// Logout
+  app.get('/auth/logout', function(req, res){
+  delete req.session.displayName;
+
+  req.session.save(function(){
+    res.redirect('/welcome');
+  });
+  //res.redirect('/welcome');
+  //res.send('Bye~ Logout successfully');
+})
+
+app.get('/welcome', function(req, res){
+  if(req.session.displayName){
+    res.send(`
+      <h2> Hello, ${req.session.displayName}</h2>
+      <a href="/auth/logout">Logout</a>
+      `);
+  } else {
+    res.send(`
+      <h2> Welcome </h2>
+      <a href="/auth/login">Login</a>
+      `);
+
+  }
+  //res.send(req.session)
+});
+
+// login
+app.post('/auth/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // db에서 사용자 인증정보를 받아와서 대조해야하지만, 편의를 위해 코드로 밖음. ㅡ.ㅡ;;
+  var user = {
+    username:'color.park',
+    password:'1234',
+    displayName:'Jospeph'
+  };
+
+  var output = '';
+
+  if(user.username==username && user.password==password){
+    req.session.displayName = user.displayName;
+    req.session.save(function(){
+      res.redirect('/welcome');
+    });
+  }
+  else{
+    res.send('Who R U ? <a href="/auth/login">login</a>');
+  }
+
+  //res.send('username: '+req.body.username+', password : '+req.body.password);
+});
+
+app.get('/auth/login', function(req, res){
+  var output = `
+    <h2> Login </h2>
+    <form action="/auth/login" method=post>
+      <p>
+          <input type='text' name='username' placeholder='username'>
+      </p><p>
+          <input type='password' name='password' placeholder='passsword'>
+      </p><p>
+        <input type='submit'>
+    </form>
+  `;
+
+  res.send(output);
+  //res.send('Hello login');
+});
+
+
+
+// LISTEN
+app.listen(3007, function(){
+    console.log('Connected 3007 port');
+});
